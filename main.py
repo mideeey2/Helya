77829db455020ee887ba9118871434cff037726f
mehdizouhir <mehdi20121@outlook.fr>
2025-12-28 21:14:18 +0100

    .

--- main.py
+++ main.py
@@ -292,10 +292,10 @@ async def mute(ctx, member:discord.Member, duration:int, raison:str="Aucun raiso
         timestamp = date.timestamp()
         await member.edit(timed_out_until=date)
         await ctx.channel.send(f"{member.mention} a été mute pendant {duration} minutes pour la raison `{raison}`.")
-        await member.send(f"Vous avez été mute sur le serveur {bot.server.name} jusqu'au <t:{int(timestamp)}:F> pour la raison `{raison}`")
+        await member.send(f"Vous avez été mute sur le serveur {bot.server.name} jusqu'au <t:{int(timestamp)}:F> pour la raison `{raison}`.")
+        await ctx.author.send(content=f"Vous avez mute {member.mention} jusqu'au <t:{int(timestamp)}:F> pour la raison `{raison}`.", ephemeral=True, view=Button(label="Annuler l'action", color=discord.ButtonStyle.green, interaction_msg=f"Vous avez annulé le mute de {member.mention}.", onclick_code=member.edit(timed_out_until=None)))
     else:
         await ctx.channel.send("Vous n'avez pas la permission d'utiliser cette commande.")
-                         
 # @bot.event
 # async def on_message(message):
 #     content = message.content[1:]
