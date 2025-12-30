1d36c69abd3ef81ac0daa86c2f4abc31ad09f760
mehdizouhir <mehdi20121@outlook.fr>
2025-12-30 14:35:59 +0100

    .

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 conn = psycopg2.connect(DATABASE_URL)
 cursor = conn.cursor()
 
-cursor.execute("DELETE TABLE vouchs")
+cursor.execute("DROP TABLE vouchs")
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS vouchs (
