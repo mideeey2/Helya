4467d899e86396fd532d85f93950c6ebcef22939
mehdizouhir <mehdi20121@outlook.fr>
2026-01-04 12:21:43 +0100

    .

--- main.py
+++ main.py
@@ -843,7 +843,7 @@ async def ticketsystem(ctx):
     if ctx.author.id == OWNER_ID:
         ticket_channel = bot.get_channel(BOTS_CHANNEL_ID)
         embed = discord.Embed(title="Création de tickets", description="Pour ouvrir un ticket, sélectionnez une raison à l'aide du sélecteur ci-dessous!", color=discord.Color.green())
-        embed.set_thumbnail(url=guild.icon.url)
+        embed.set_thumbnail(url=ctx.guild.icon.url)
         embed.author(ctx.guild.name)
         embed.set_footer(text="Merci de ne pas créer des tickets sans raison!", icon_url=ctx.guild.icon.url)
         await ticket_channel.send(embed=embed, view=TicketReasonView())
