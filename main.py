857e8413d6b7d719b05c79976dbed9be44f43462
mideeey2 <zouhirmehdi508@gmail.com>
2026-02-02 20:53:32 +0100

    .

--- main.py
+++ main.py
@@ -1173,6 +1173,19 @@ async def reglement(ctx):
     message = await ctx.send(embed=embed)
     await message.add_reaction("<a:oui:1467958627901112390>")
 
+@bot.command()
+async def roleschoice(ctx):
+    if ctx.author.id == OWNER_ID:
+        await ctx.message.delete()
+        embed = discord.Embed(title="Rôles de notifications", description="Pour récupérer tes rôles de notifications, je t'invite à cliquer sur les réactions ci-dessous!\nVoici les rôles :\n↪<@&1467961939123507222>\n↪<@&1467962712863539420>\n↪<@&1467963534448132248>\n↪<@&1467962551789818033>\n↪<@&1467962389549940849>\n↪<@&1467962148058562592>")
+        message = await ctx.send(embed=embed)
+        message.add_reaction("🎉")
+        message.add_reaction("🎨")
+        message.add_reaction("📊")
+        message.add_reaction("🔊")
+        message.add_reaction("🗨️")
+        message.add_reaction("🤝")
+
 # @bot.command()
 # async def roleicon(ctx, *, args):
 #     role_mentions=ctx.message.role_mentions
